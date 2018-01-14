b35890b8cc94beb3eba95311004ba3c0860a0a99
wzbll <wzb121318@163.com>
2018-01-14 20:03:46 +0800

    git提交测试

---
+++ wscp-web/src/test/java/com/git/wacp/service/testA.java
@@ -0,0 +1,24 @@
+package com.git.wacp.service;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class testA {
+    public static void main(String[] arg) {
+        AtomicInteger countA = new AtomicInteger(0);
+        AtomicInteger countB = new AtomicInteger(0);
+
+        if(countA == countB){
+            System.out.print("This is True");
+        }
+        if(countA.equals(countB)){
+            System.out.print("This is False");
+        }
+
+        final Lock lock = new ReentrantLock();
+        final Condition full = lock.newCondition();
+
+    }
+}
